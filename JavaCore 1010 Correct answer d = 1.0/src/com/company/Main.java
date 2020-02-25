10267b0ea5d8816931f98d8ad9123418dfc19dba
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-25 10:43:10 +0200

    Add files via upload

---
+++ JavaCore 1010 Correct answer d = 1.0/src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+/*
+
+1010 Correct answer: d = 1.0
+Add one type conversion operation to get the answer: d = 1.0.
+
+Requirements:
+1. The program should display the text on the screen.
+2. You cannot change the display command.
+3. The main () method must contain a variable of type int.
+4. The main () method must contain a variable b of type int.
+5. The main () method must contain a variable c of type int.
+6. The main () method must contain an e variable of type int.
+7. The main () method must contain a d variable of type double.
+8. The initial value of the variables during initialization cannot be changed. You can add only cast operators.
+9. The program should output the number 1.0.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 257;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d =(byte)a + b / c / e;
+
+        System.out.println(d);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
